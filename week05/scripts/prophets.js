a3c20d11f8464c0041ac0671b92280b3313391d7
sundayjohn78 <olorunferanmisunday@gmail.com>
2024-05-23 19:14:30 -0700

    week05

--- week05/scripts/prophets.js
+++ week05/scripts/prophets.js
@@ -5,30 +5,39 @@ async function getProphetData(url) {
     const response = await fetch(url);
     const data = await response.json();
     console.table(data.prophets);
-    //displayProphets(data.prophets); // note that we reference the prophets array of the JSON data object, not just the object
-  }
-  
-  getProphetData();
+    displayProphets(data.prophets); // Display the prophets data on the page
+}
 
-/*const displayProphets = (prophets) => {
+const displayProphets = (prophets) => {
     prophets.forEach((prophet) => {
         // Create elements to add to the div.cards element
         let card = document.createElement('section');
-        let fullName = document.createElement('__'); // fill in the blank
+        let fullName = document.createElement('h2'); // Correct element is 'h2'
+        let dateOfBirth = document.createElement('h3');
+        let placeOfBirth = document.createElement('h3');
         let portrait = document.createElement('img');
         
         // Build the h2 content out to show the prophet's full name
-        fullName.textContent = `${prophet._____} ____________`; // fill in the blank
+        fullName.textContent = `${prophet.name} ${prophet.lastname}`; // Correct properties are 'name' and 'lastname'
+        dateOfBirth.textContent = `Date of Birth: ${prophet.birthdate}`;
+        placeOfBirth.textContent = `place of Birth: ${prophet.birthplace}`;
+
+
         // Build the image portrait by setting all the relevant attributes
         portrait.setAttribute('src', prophet.imageurl);
-        portrait.setAttribute('alt', `Portrait of ${prophet.____} ______________`); // fill in the blank
+        portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`); // Correct properties are 'name' and 'lastname'
         portrait.setAttribute('loading', 'lazy');
         portrait.setAttribute('width', '340');
         portrait.setAttribute('height', '440');
         
         // Append the section(card) with the created elements
-        card.appendChild(_______); //fill in the blank
+        card.appendChild(fullName); // Correct variable is 'fullName'
+        card.appendChild(dateOfBirth);
+        card.appendChild(placeOfBirth);
         card.appendChild(portrait);
         cards.appendChild(card);
     }); // end of arrow function and forEach loop
-}*/
+}
+
+// Fetch and display the data
+getProphetData(url);
